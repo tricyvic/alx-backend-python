ea65afda42262f38139c4891647f3e677caa3201
Victor Muhita <wangombevictor5@gmail.com>
2025-07-11 16:46:19 +0300

    use async piython

--- python-context-async-perations-0x02/3-concurrent.py
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -2,19 +2,20 @@ import asyncio
 import aiosqlite
 
 #  async_fetch_users() and async_fetch_older_users()
-async def async_fetch_users(db, query):
-    async with db.execute(query) as cursor:
+async def async_fetch_users():
+    db = await aiosqlite.connect("users.db")
+    async with db.execute("SELECT * FROM users") as cursor:
         return await cursor.fetchall()
 
-async def async_fetch_older_users(db, age):
-    query = "SELECT * FROM users WHERE age > ?"
-    async with db.execute(query, (age,)) as cursor:
+async def async_fetch_older_users():
+    db = await aiosqlite.connect("users.db")
+    async with db.execute("SELECT * FROM users WHERE age > ?", (40,)) as cursor:
         return await cursor.fetchall()
 
 async def main():
     coroutine_objects = [
-        async_fetch_users(aiosqlite.connect("users.db"),"SELECT * FROM users"),
-        async_fetch_older_users(aiosqlite.connect("users.db"),40),
+        async_fetch_users(),
+        async_fetch_older_users(),
     ]
 
     results = await asyncio.gather(*coroutine_objects)
