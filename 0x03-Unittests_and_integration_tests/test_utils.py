f1f76571c4816f8ec9f15b21692ff96616352242
Victor Muhita <wangombevictor5@gmail.com>
2025-07-19 15:04:40 +0300

    add test mock

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import unittest
-from utils import access_nested_map
+import requests
+from unittest.mock import patch,Mock
+from utils import access_nested_map,get_json
 from parameterized import parameterized
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -20,4 +22,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as x:
             access_nested_map(nested_map, path)
+
+class TestGetJson(unittest.TestCase):
+    @patch(requests.get)
+    def test_get_json(self,mock_get):
+        mock_res = Mock()
+        res_dict = {"payload": True}
+        mock_res.json.return_value = res_dict
+
+        mock_get.return_value = mock_res
+
+        mock_get.assert_called_with("http://example.com")
+
+        self.assertEqual(get_json("http://example.com"),res_dict)
 
