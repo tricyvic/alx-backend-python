cc9bce3be86d1535ad6b9063c8602202d2b99c16
Victor Muhita <wangombevictor5@gmail.com>
2025-07-19 16:16:23 +0300

    add views

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
-    # should contain: ["api-auth"]
+    # should contain: api-auth"
 ]
